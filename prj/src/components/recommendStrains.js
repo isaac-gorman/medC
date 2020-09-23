b2cedcb32b2390fa09d9b8b2ffad00c341ae0bc5
Isaac Gorman <emailme@isaacg.net>
2020-09-22 20:24:18 -0500

    bang

---
+++ prj/src/components/recommendStrains.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const recommendStrains = () => {
+  return (
+    <div>
+      {/* Q: What do I need to render in this component?*/}
+      {/* - I need to reco */}
+      <RecomendationContainer>{}</RecomendationContainer>
+    </div>
+  );
+};
+
+export default recommendStrains;
---
+++ prj/src/components/savedStrains.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    id: 0,
+    strain_name: "",
+    top_rated: true,
+    treatment_type: "Anxiety Treatment",
+    price: "00.00",
+    consumption_methods: ["Inhalation", "Ingestion", "Application"],
+  },
+];
